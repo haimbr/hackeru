cfa23dda3021a0aebd7c46f5cafd5a022d6b47b5
haimbr97 <92398770+haimbr97@users.noreply.github.com>
2021-10-14 10:30:08 +0300

    clear the code

--- hackeru.js
+++ hackeru.js
@@ -1,37 +1,36 @@
 
 
-
 // handle with the hidden nav
 
 let headers = document.querySelectorAll('.main-header-item');
 let hiddenNav = document.querySelectorAll('.hidden-nav');
 let body = document.querySelector('body');
-body.addEventListener('click',(event) =>{
+body.addEventListener('click', (event) => {
     handleNavClickEvents(event);
 })
-function handleNavClickEvents(event){
+function handleNavClickEvents(event) {
     let currentNavItem;
-    if(event.target.classList != null && event.target.classList.contains('main-header-item')){
-        for(let i=1; i<headers.length-2; i++){
-            if(event.target == headers[i] && (hiddenNav[i-1].style.display != 'grid' && hiddenNav[i-1].style.display != 'block')){
-                if(body.offsetWidth > 1018)
-                    hiddenNav[i-1].style.display = 'grid';
-                else 
-                    hiddenNav[i-1].style.display = 'block';
-                currentNavItem = i-1;
+    if (event.target.classList != null && event.target.classList.contains('main-header-item')) {
+        for (let i = 1; i < headers.length - 2; i++) {
+            if (event.target == headers[i] && (hiddenNav[i - 1].style.display != 'grid' && hiddenNav[i - 1].style.display != 'block')) {
+                if (body.offsetWidth > 1018)
+                    hiddenNav[i - 1].style.display = 'grid';
+                else
+                    hiddenNav[i - 1].style.display = 'block';
+                currentNavItem = i - 1;
             }
-            
-        }        
-    }   
-    for(let i=0; i<hiddenNav.length; i++){
-        if(i!=currentNavItem)
-        hiddenNav[i].style.display = "none";   
-    }  
+
+        }
+    }
+    for (let i = 0; i < hiddenNav.length; i++) {
+        if (i != currentNavItem)
+            hiddenNav[i].style.display = "none";
+    }
 }
 
 
 // handle with the scrolled text.
-const scrolledText =  document.querySelector(' .inner > p');
+const scrolledText = document.querySelector(' .inner > p');
 const scrollTextContainer = document.querySelector('.scroll-text-container');
 const container = scrollTextContainer.querySelector('.inner');
 
@@ -51,32 +50,32 @@ scrollLoop();
 
 function scrollLoop() {
     progress = progress - speed;
-    if(progress <= elWidth * -1) {
-    progress = 0;
-    }        
-      container.style.transform = 'translateX(' + -progress + 'px)';             
-      window.requestAnimationFrame(scrollLoop);                      
-}   
-    // stops the scroll on mouse over
-function addScrollEventListener(){
-    scrolledText.addEventListener('mouseover', function(){
-      speed = 0;   
-    })        
-    scrolledText.addEventListener('mouseout', function(){
-      speed = 1;   
+    if (progress <= elWidth * -1) {
+        progress = 0;
+    }
+    container.style.transform = 'translateX(' + -progress + 'px)';
+    window.requestAnimationFrame(scrollLoop);
+}
+// stops the scroll on mouse over
+function addScrollEventListener() {
+    scrolledText.addEventListener('mouseover', function () {
+        speed = 0;
     })
-}     
+    scrolledText.addEventListener('mouseout', function () {
+        speed = 1;
+    })
+}
 
 
-    // handle with the video 
+// handle with the video 
 let videoLink = document.querySelector('.video-link_container');
 let backdrop = document.querySelector('.backdrop');
 let video = document.querySelector('.video');
-videoLink.addEventListener('click' , function(){
+videoLink.addEventListener('click', function () {
     video.style.display = 'block';
     backdrop.style.display = 'block';
 })
-backdrop.addEventListener('click' , function(){
+backdrop.addEventListener('click', function () {
     video.style.display = 'none';
     backdrop.style.display = 'none';
     var iframes = document.getElementsByTagName("iframe");
@@ -91,19 +90,19 @@ let elem = document.querySelectorAll('.process_element');
 let elem2 = document.querySelector('html');
 let animatedElements = document.querySelectorAll('.animated-elements')
 let processElementOpened = false;
-window.addEventListener('scroll', function(){ handleScrollEvent();}, true);
+window.addEventListener('scroll', function () { handleScrollEvent(); }, true);
 
-function  handleScrollEvent(){
-    if(elem2.scrollTop > 1400 && processElementOpened == false){
-        for(let i=0; i<elem.length; i++){
-            setTimeout(function(){elem[i].classList.add('transitionEffect'); }, i*500); 
+function handleScrollEvent() {
+    if (elem2.scrollTop > 1400 && processElementOpened == false) {
+        for (let i = 0; i < elem.length; i++) {
+            setTimeout(function () { elem[i].classList.add('transitionEffect'); }, i * 500);
         }
         processElementOpened = true;
-    } 
-    for(let i=0; i<animatedElements.length; i++){
-        if(elem2.scrollTop + window.screen.height > animatedElements[i].offsetTop)
-        animatedElements[i].classList.add('transitionEffect');
-    }  
+    }
+    for (let i = 0; i < animatedElements.length; i++) {
+        if (elem2.scrollTop + window.screen.height > animatedElements[i].offsetTop)
+            animatedElements[i].classList.add('transitionEffect');
+    }
 }
 
 
@@ -111,40 +110,37 @@ function  handleScrollEvent(){
 let navContainer = document.querySelector('.main-nav-ul');
 let headerIcon = document.querySelectorAll('.header-icon');
 let headerIconContainer = document.querySelector('.header-icon-container');
-headerIconContainer.addEventListener('click',() =>{
-if(!headerIcon[0].classList.contains('transition-a')){
-    headerIcon[0].classList.add('transition-a');
-    headerIcon[1].classList.add('transition-c');
-    headerIcon[2].classList.add('transition-b');
-    navContainer.style.display = 'block';
-}
-else{
-    headerIcon[0].classList.remove('transition-a');
-    headerIcon[1].classList.remove('transition-c');
-    headerIcon[2].classList.remove('transition-b');
-    navContainer.style.display = 'none';
-}
+headerIconContainer.addEventListener('click', () => {
+    if (!headerIcon[0].classList.contains('transition-a')) {
+        headerIcon[0].classList.add('transition-a');
+        headerIcon[1].classList.add('transition-c');
+        headerIcon[2].classList.add('transition-b');
+        navContainer.style.display = 'block';
+    }
+    else {
+        headerIcon[0].classList.remove('transition-a');
+        headerIcon[1].classList.remove('transition-c');
+        headerIcon[2].classList.remove('transition-b');
+        navContainer.style.display = 'none';
+    }
 })
 
 
 
 
 let popUp = document.querySelector('.pop-up-container');
-function openPopUp(){
+function openPopUp() {
     popUp.style.display = 'inline-block';
-    // setTimeout(() => {popUp.classList.add('transitionEffect');}, 1600);
-    // popUp.style.transform = 'translateY(0px)';
-    vv();
+    addTransitionEffect();
     backdrop.style.display = 'block';
 }
 let xIcon = document.querySelector('.x-icon');
-xIcon.addEventListener('click', ()=>{
+xIcon.addEventListener('click', () => {
     popUp.style.display = 'none';
     backdrop.style.display = 'none';
 })
 
 
-function vv(){
-    setTimeout(() => {popUp.classList.add('transitionEffect');}, 1600);
+function addTransitionEffect() {
+    setTimeout(() => { popUp.classList.add('transitionEffect'); }, 1600);
 }
-
